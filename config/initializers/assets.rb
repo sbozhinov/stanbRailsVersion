2c5ba2e466271368e0d290087d80351645372498
sbozhinov <sbozhinov@msn.com>
2016-04-17 02:58:32 -0700

    ims

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( styles.css.erb )
 Rails.application.config.assets.precompile += %w( myscript.js )
 Rails.application.config.assets.precompile += %w( lightgallery.css )
