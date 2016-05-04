2cc0d95a6cfc8b736b033da17ba129c9b255db86
sbozhinov <sbozhinov@msn.com>
2016-05-03 17:05:38 -0700

    background

--- test/controllers/portfolio_controller_test.rb
+++ test/controllers/portfolio_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PortfolioControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :portfolio
+    assert_response :success
+  end
 end
